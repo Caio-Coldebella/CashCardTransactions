3be4cf90e2cd95c4f3a0fee539ce9988542e3c16
Caio Coldebella <caio.r.coldebella@outlook.com>
2024-10-02 00:43:20 -0300

    feat: add global exception handler

---
+++ src/main/java/course/cashcard/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package course.cashcard.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
